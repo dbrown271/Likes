c9c9d2efb6bcbdda3509319969f13a4b25d218c8
dbrown271 <d.brown271@icloud.com>
2021-09-21 21:02:57 -0500

    intial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Count 1
+var count = 1;
+var countelement = document.querySelector("#count")
+
+
+function add() {
+    count++;
+    countelement.innerText = count + " like(s)";
+    console.log(count)
+}
+
+// Count 2
+var count2 = 1;
+var count2element = document.querySelector("#count2")
+
+
+function add2() {
+    count2++;
+    count2element.innerText = count2 + " like(s)";
+    console.log(count2)
+}
+
+// Count 3
+var count3 = 1;
+var count3element = document.querySelector("#count3")
+
+
+function add3() {
+    count3++;
+    count3element.innerText = count3 + " like(s)";
+    console.log(count3)
+}
